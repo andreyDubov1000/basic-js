d4795b21cb57c7a58fb93efd7266e690f5ed3fb5
Andrey Dubov <andreydubov1000@gmail.com>
2021-09-16 17:20:32 +0300

    9 tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,23 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default
+  function repeater(str, options){
+    let strResult=''
+    let mainStr = String(str)
+    let n = options.hasOwnProperty('repeatTimes')?options.repeatTimes:1
+    let sep = options.hasOwnProperty('separator')?String(options.separator):'+'
+    let add = options.hasOwnProperty('addition')?String(options.addition):''
+    let addSep = options.hasOwnProperty('additionSeparator')?String(options.additionSeparator):'|'
+    let m = options.hasOwnProperty('additionRepeatTimes')?options.additionRepeatTimes:1
+    for (let i=0; i<n;i++){
+      strResult+=mainStr
+      for (let j=0;j<m;j++) {
+        strResult+=(j<m-1)?add+addSep:add
+      }
+      strResult+=(i<n-1)?sep:''
+    }
+
+    return strResult
+
+  }
