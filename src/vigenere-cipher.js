a0486dcc26c4d82ea66475f88c33799493d68259
Andrey Dubov <andreydubov1000@gmail.com>
2021-09-17 17:03:23 +0300

    9 tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,52 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (s = true){
+    this.square = []
+    this.alf = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("")
+    for (let i = 0; i < 26; i++) {
+      this.square[i] = this.alf.slice(i).concat(this.alf.slice(0, i));
+    }
+    const crypt= (text,key)=> {
+      if (!(text&&key)){throw new Error('Incorrect arguments!')}
+      let s = "";
+      const regexp = /[A-Z]/
+      const textUp = text.toUpperCase()
+      let keyUp = key.toUpperCase()
+      while (textUp.length>keyUp.length) {keyUp+=keyUp}
+      for (let i = 0,j=0; i < text.length; i++,j++) {
+        s+= (regexp.test(textUp[i])) ?
+            this.square[this.alf.indexOf(textUp[i])][this.alf.indexOf(keyUp[j])]:
+            ( j--,textUp[i])
+      }
+      return s;
+    }
+    const exCrypt= (text,key)=> {
+      if (!(text&&key)){throw new Error('Incorrect arguments!')}
+      let s = "";
+      const regexp = /[A-Z]/
+      const textUp = text.toUpperCase()
+      let keyUp = key.toUpperCase()
+      while (textUp.length>keyUp.length) {keyUp+=keyUp}
+      for (let i = 0,j=0; i < text.length; i++,j++) {
+        s+= (regexp.test(textUp[i])) ?
+            this.alf[this.square[this.alf.indexOf(keyUp[j])].indexOf(textUp[i])]:
+            ( j--,textUp[i])
+      }
+      return s;
+    }
+    switch (s) {
+      case true:
+        this.encrypt=crypt
+        this.decrypt=exCrypt
+        break
+      case false:
+        this.encrypt=(text,key)=>crypt(text,key).split('').reverse().join('')
+        this.decrypt=(text,key)=>exCrypt(text,key).split('').reverse().join('')
+        break
+      default:
+        throw new Error('Incorrect Class key')
+    }
+
   }
 }
