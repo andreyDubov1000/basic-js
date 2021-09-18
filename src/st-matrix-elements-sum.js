802e04fb8c9063888bc587ea4795a0af4e438aaf
Andrey Dubov <andreydubov1000@gmail.com>
2021-09-18 18:44:55 +0300

    19 tasks

--- src/st-matrix-elements-sum.js
+++ src/st-matrix-elements-sum.js
@@ -16,7 +16,12 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be 9
  */
-export default function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getMatrixElementsSum(matrix){
+  let result=matrix[0].reduce((s, item) => item + s, 0)
+  for (let i=1;i<matrix.length;i++){
+    for (let j=0;j<matrix[i].length;j++){
+      result+=(matrix[i-1][j])?matrix[i][j]:0
+    }
+  }
+  return result
 }
--- src/st-sort-by-height.js
+++ src/st-sort-by-height.js
@@ -11,7 +11,8 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-export default function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function sortByHeight(arr){
+  const sort=arr.filter((item)=>item!==-1).sort((a,b)=>a-b)
+  let i=0
+  return arr.map((item,index)=>(item===-1)?item:(sort[i++]))
 }
